6e7e52981705cc68b0af79b6cb13f2017a72f7a4
PFletcher <PFletcher>
2016-08-03 18:55:49 -0400

    created user model

---
+++ models/user.js
@@ -0,0 +1,27 @@
+
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  firstName:{
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+    unique: true
+  }
+});
+
+var User = mongoose.model('User', commentSchema);
+module.exports = User;
