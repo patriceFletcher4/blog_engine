e4e20775001d174db81dda353ca5781b53b787f3
PFletcher <PFletcher>
2016-08-05 20:45:53 -0400

    created user router

---
+++ routes/users.js
@@ -0,0 +1,95 @@
+
+
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user.js');
+
+
+router.get('/users', getAllUsers);
+router.get('/users/:userId', getOneUser);
+router.post('/users', createUser);
+router.delete('/users/:userId', deleteUser);
+router.put('/users/:userId', updateUser);
+
+module.exports = router;
+
+function getAllUsers(req, res, next){
+  var find({user: req.params.userId}, function(err, users){
+    if(err) {
+      res.status(500).json({
+        msg: err
+      })
+    } else {
+      if(users){
+        res.status(200).json({
+          users: users
+        });
+      } else {
+        res.status(404).json({
+          mgs: "couldn't find user"
+        });
+       }
+     }
+  });
+}
+function getOneUser(req, res, next){
+  var find({_userId, req.params.userId}, function(err, foundOneUser){
+    if(err){
+      res.status(500).json({
+        msg: err
+      })
+    } else {
+      res.status(200).json({
+        users: foundOneUser
+      });
+    }
+  })
+}
+
+function createUser(req, res, next){
+  var user = new User({
+    title: req.body.title,
+    author: req.body.author,
+    body: req.body.body,
+    created: new Date(),
+  });
+  user.findOne(function(err, createdUser){
+    if(err){
+      res.status(500).json({
+        msg:err
+      });
+    } else {
+      res.status(201).json({
+        createdUser: newUser
+      });
+    }
+  });
+}
+
+function updateUser(req, res, next){
+  User.findOneAndUpdate({_id: req.params.id}, req.body, function(err, user){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        user: user
+      });
+    }
+  });
+}
+
+function deleteUser(req, res, next){
+  User.findOneAndRemove({_id: req.params.id}, req.body, function(err, deleteUser){
+    if(err){
+      res.status(500).json({
+        msg:err
+      });
+    } else {
+      res.status(201).json({
+        removedUser: deleteUser
+      });
+    }
+  });
+}
