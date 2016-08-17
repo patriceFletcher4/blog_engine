d8fc73c4cb6139edeb4744eb9b25c1da5efc9887
PFletcher <PFletcher>
2016-08-16 20:25:20 -0400

    implemented passport to check login

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
-
-
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var crypto = require('crypto');
 
 var userSchema = new Schema({
   firstName:{
@@ -20,8 +19,30 @@ var userSchema = new Schema({
     type: String,
     required: true,
     unique: true
+  },
+  hash: {
+    type: String,
+    required: true,
+    uniqie: false
+  },
+  salt: {
+    type: String,
+    required: false
   }
 });
+userSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+                    .toString('hex');
+}
+
+userSchema.methods.vaildPassword = function(password){
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+                  .toString('hex');
+  return this.hash === hash;
+}
+
+userSchema.methods.generateJwt = function(){}
 
 var User = mongoose.model('User', userSchema);
 module.exports = User;
